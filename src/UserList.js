88b109d3772e4a6692ddb3c08816375c49b61683
EtErNaLiFe12 <wnsqja8888@gmail.com>
2021-10-13 23:39:23 +0900

    useEffect 관련

--- src/UserList.js
+++ src/UserList.js
@@ -1,7 +1,20 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 function User({ user, onRemove, onToggle }) {
   const { username, email, id, active } = user;
+  useEffect(() => {
+    console.log('컴포넌트가 화면에 나타남');
+    // props -> state
+    // REST API
+    // LIBRARY
+    // setInterval, setTimeout
+    return () => {
+      console.log('컴포넌트가 화면에서 사라짐');
+      //setInterval, setTimeout, clearTimeout, clearInterval
+      // 라이브러리 인스턴스 제거
+    }
+  },[]);
+  // }, [] = 배열에 들어가는 값은 deps로 의존성 값을 넣을 수 있음, 넣은 값들은 바뀌거나 변경될때 마다 호출됨);
   return (
     <div>
       <b style = {{
